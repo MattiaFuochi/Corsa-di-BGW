5377ababdb6364280acbbfd3825a0e62fceb0440
Mattia Fuochi <mattia.fuochi.studenti@isii.it>
2020-11-20 15:53:56 +0100

    Aggiungere i file di progetto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Corsa_di_BGW
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAvvia = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.bgw1 = new System.ComponentModel.BackgroundWorker();
+            this.bgw2 = new System.ComponentModel.BackgroundWorker();
+            this.Display = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pbRed = new System.Windows.Forms.PictureBox();
+            this.pbBlack = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBlack)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAvvia
+            // 
+            this.btnAvvia.Location = new System.Drawing.Point(123, 348);
+            this.btnAvvia.Name = "btnAvvia";
+            this.btnAvvia.Size = new System.Drawing.Size(176, 81);
+            this.btnAvvia.TabIndex = 0;
+            this.btnAvvia.Text = "button1";
+            this.btnAvvia.UseVisualStyleBackColor = true;
+            this.btnAvvia.Click += new System.EventHandler(this.btnAvvia_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(508, 348);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(158, 81);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "button2";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // bgw1
+            // 
+            this.bgw1.WorkerReportsProgress = true;
+            this.bgw1.WorkerSupportsCancellation = true;
+            this.bgw1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.Bgw_DoWork);
+            this.bgw1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.Bgw_ProgressChanged);
+            this.bgw1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.Bgw_RunWorkerCompleted);
+            // 
+            // bgw2
+            // 
+            this.bgw2.WorkerReportsProgress = true;
+            this.bgw2.WorkerSupportsCancellation = true;
+            this.bgw2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.Bgw_DoWork);
+            this.bgw2.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.Bgw_ProgressChanged);
+            this.bgw2.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.Bgw_RunWorkerCompleted);
+            // 
+            // Display
+            // 
+            this.Display.Location = new System.Drawing.Point(12, 282);
+            this.Display.Multiline = true;
+            this.Display.Name = "Display";
+            this.Display.Size = new System.Drawing.Size(776, 36);
+            this.Display.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Corsa_di_BGW.Properties.Resources.road_trip_background_10864211;
+            this.pictureBox1.Location = new System.Drawing.Point(1, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(798, 275);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pbRed
+            // 
+            this.pbRed.Image = global::Corsa_di_BGW.Properties.Resources.macchinarossa;
+            this.pbRed.Location = new System.Drawing.Point(1, 166);
+            this.pbRed.Name = "pbRed";
+            this.pbRed.Size = new System.Drawing.Size(150, 77);
+            this.pbRed.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbRed.TabIndex = 4;
+            this.pbRed.TabStop = false;
+            // 
+            // pbBlack
+            // 
+            this.pbBlack.Image = global::Corsa_di_BGW.Properties.Resources.macchinabianca;
+            this.pbBlack.Location = new System.Drawing.Point(0, 28);
+            this.pbBlack.Name = "pbBlack";
+            this.pbBlack.Size = new System.Drawing.Size(151, 88);
+            this.pbBlack.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbBlack.TabIndex = 5;
+            this.pbBlack.TabStop = false;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pbBlack);
+            this.Controls.Add(this.pbRed);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Display);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnAvvia);
+            this.Name = "FrmMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBlack)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAvvia;
+        private System.Windows.Forms.Button btnReset;
+        private System.ComponentModel.BackgroundWorker bgw1;
+        private System.ComponentModel.BackgroundWorker bgw2;
+        private System.Windows.Forms.TextBox Display;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pbRed;
+        private System.Windows.Forms.PictureBox pbBlack;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Corsa_di_BGW
+{
+    public partial class FrmMain : Form
+    {
+        Random r = new Random();
+        const int arrivo = 648;
+        const int inizio = 0;
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+        private void FrmMain_Load(object sender, EventArgs e)
+        {       
+            this.Text = "Corsa di BGW";
+            btnAvvia.Text = "START";
+            btnReset.Text = "RESET";
+            Display.Text ="vincitore:";
+            pbBlack.Location = new Point(inizio, pbBlack.Location.Y);
+            pbRed.Location = new Point(inizio, pbRed.Location.Y);
+
+        }
+        private void btnAvvia_Click(object sender, EventArgs e)
+        {
+            btnAvvia.Enabled = false;
+            bgw1.RunWorkerAsync(pbBlack.Location.X);
+            bgw2.RunWorkerAsync(pbRed.Location.X);
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            Display.Text = "Vincitore:";
+            bgw1.CancelAsync();
+            bgw2.CancelAsync();
+            btnAvvia.Enabled = true;
+            pbBlack.Location = new Point(inizio, pbBlack.Location.Y);
+            pbRed.Location = new Point(inizio, pbRed.Location.Y);
+        }
+
+        private void Bgw_DoWork(object sender, DoWorkEventArgs e)
+        {
+           BackgroundWorker bw = sender as BackgroundWorker;
+            int posizione = (int)e.Argument;
+
+            while (posizione < arrivo)
+            {
+                if (bw.CancellationPending)
+                    return;
+
+                posizione += r.Next(1, 5);
+                bw.ReportProgress(posizione);
+
+                System.Threading.Thread.Sleep(10);
+            }
+
+            if (bw == bgw1)
+                e.Result = "Nero";
+            else
+                e.Result = "Rosso";
+        }
+
+        private void Bgw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            BackgroundWorker bw = sender as BackgroundWorker;
+            int pos = (int)e.ProgressPercentage;
+
+            if (bw == bgw1)
+            {
+                if (pos > arrivo)
+                    pbBlack.Location = new Point(arrivo, pbBlack.Location.Y);
+                else
+                    pbBlack.Location = new Point(e.ProgressPercentage, pbBlack.Location.Y);
+            }
+            else
+            {
+                if (pos > arrivo)
+                    pbRed.Location = new Point(arrivo, pbRed.Location.Y);
+                else
+                    pbRed.Location = new Point(e.ProgressPercentage, pbRed.Location.Y);
+            }
+        }
+
+        private void Bgw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            BackgroundWorker bw = sender as BackgroundWorker;
+            Display.Text += (string)e.Result;
+            if (bw == bgw1)
+                bgw2.CancelAsync();
+            else
+                bgw1.CancelAsync();
+        }
+    }
+}
